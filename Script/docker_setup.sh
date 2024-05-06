d8b13f6772c6c086e08f14b7d1d117a2230c6eb8
Jeff <131836756+Pang-Duck@users.noreply.github.com>
2024-05-06 18:22:18 +0900

    update dockerset up

--- Script/docker_setup.sh
+++ Script/docker_setup.sh
@@ -2,7 +2,7 @@
 # root 권한으로 실행
 
 OS_CHECK="$(cat /proc/version)"
-GPU_CHECK="$(lspci | grep NVIDIA| wc -l)"
+GPU_CHECK="$(lspci | grep NVIDIA | wc -l)"
 
 if [[ "$OS_CHECK" =~ "ubuntu" ]] || [[ "$OS_CHECK" =~ "Ubuntu" ]]; then
     sudo apt update
@@ -15,7 +15,7 @@ if [[ "$OS_CHECK" =~ "ubuntu" ]] || [[ "$OS_CHECK" =~ "Ubuntu" ]]; then
     sudo service docker restart
     echo "Docker for Ubuntu download complete!"
 
-elif [[ "$OS_CHECK" =~ "centos" ]] [[ "$OS_CHECK" =~ "rocky"]]; then
+elif [[ "$OS_CHECK" =~ "centos" ]] || [[ "$OS_CHECK" =~ "rocky" ]]; then
     sudo yum install -y yum-utils
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io
@@ -27,9 +27,9 @@ else
     echo "Check OS version is ubuntu or centos"
 fi
 
-if [ "$GPU_CHECK" -eq 0] ; then
+if [ "$GPU_CHECK" -eq 0 ]; then
 
-    cat << EOF > /etc/docker/daemon.json
+    sudo cat <<EOF >/etc/docker/daemon.json
 {
 "data-root": "/var/lib/docker",
 "storage-driver": "overlay2",
@@ -40,19 +40,22 @@ if [ "$GPU_CHECK" -eq 0] ; then
     "max-file": "30"
 }
 }
-EOF    
+EOF
+
     sudo systemctl restart docker
     echo "Docker daemon setting complete!!"
 
 elif [ "$GPU_CHECK" -ne 0 ]; then
 
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+    distribution=$(
+        . /etc/os-release
+        echo $ID$VERSION_ID
+    )
     curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
     curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
     sudo apt update && apt install -y nvidia-container-toolkit
-    sudo systemctl restart docker
 
-    cat << EOF > /etc/docker/daemon.json
+    sudo cat <<EOF >/etc/docker/daemon.json
 {
 "data-root": "/var/lib/docker",
 "storage-driver": "overlay2",
